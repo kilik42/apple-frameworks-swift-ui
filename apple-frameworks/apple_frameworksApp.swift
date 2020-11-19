d865a31b7002339763dcb7a2d2faaa53c5f92a7e
marvin evins <marvinevins@yahoo.com>
2020-11-19 07:12:03 -0600

    Initial Commit

---
+++ apple-frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple-frameworks
+//
+//  Created by marvin evins on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
