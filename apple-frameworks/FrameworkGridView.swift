196f64f02057e3de7174942f4d476b9a9e585526
marvin evins <marvinevins@yahoo.com>
2020-11-21 08:42:37 -0600

    refactoring gridframe

--- apple-frameworks/FrameworkGridView.swift
+++ apple-frameworks/FrameworkGridView.swift
@@ -19,7 +19,7 @@ struct FrameworkGridView: View {
             
             ForEach(MockData.frameworks, id: \.id){
                 framework in
-                FrameworkTitleView(name: framework.name, imageName: framework.imageName)
+                FrameworkTitleView(framework: framework)
             }
             
            
@@ -37,15 +37,14 @@ struct FrameworkGridView_Previews: PreviewProvider {
 
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View{
         VStack{
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width:90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
